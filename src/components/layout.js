56ac8da79123aaa5563dff91b2104a212a97a124
Sampsoni <sami.repo@live.com>
2020-04-21 21:46:17 +0300

    Added Assets folder. Added a free css / scss template. Removed page2

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,8 +9,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
 
+import "../assets/vendor/nucleo/css/nucleo.css";
+import "../assets/vendor/font-awesome/css/font-awesome.min.css";
+import "../assets/scss/argon-design-system-react.scss?v1.1.0";
+
 import Header from "./header";
-import "./layout.css";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
