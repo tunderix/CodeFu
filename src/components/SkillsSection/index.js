fe2e2e9206c7e69c6c4650dd3b775746be757927
Sampsoni <sami.repo@live.com>
2020-04-22 00:28:31 +0300

    Added Skill Sections

--- src/components/SkillsSection/index.js
+++ src/components/SkillsSection/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import SkillCategory from "./SkillCategory";
 
 const Skills = styled.div``;
-
+const SkillsContainer = styled.div``;
 const SkillsSection = () => (
   <Skills>
     <h1>Skills</h1>
+    <SkillsContainer className="container">
+      <div className="row">
+        <SkillCategory></SkillCategory>
+        <SkillCategory></SkillCategory>
+        <SkillCategory></SkillCategory>
+      </div>
+    </SkillsContainer>
   </Skills>
 );
 
